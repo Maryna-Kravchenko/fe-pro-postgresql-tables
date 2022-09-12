98125989c895c629fba11ab3cdd4bdb30e368794
mkravchenko-2022 <mkravchenko@xmentium.com>
2022-09-11 19:59:53 -0600

    for check

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ export const initConnection = () => {
   const client = new Client({
     user: POSTGRES_USER || 'postgres',
     host: POSTGRES_HOST || 'localhost',
-    database: POSTGRES_DB || 'postgres',
-    password: POSTGRES_PASSWORD || 'postgres',
-    port: POSTGRES_PORT || 5556,
+    database: POSTGRES_DB || 'hw22',
+    password: POSTGRES_PASSWORD || 'kma23011985',
+    port: POSTGRES_PORT || 5432,
   });
 
   return client;
@@ -23,8 +23,32 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
-  // Your code is here...
+  await client.query(
+    `DROP TABLE if exists books, authors, categories, users, descriptions, reviews`
+  );
+
+  await client.query(
+    `CREATE TABLE users (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(30) NOT NULL, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)`
+  );
+  await client.query(
+    `CREATE TABLE categories (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(30) NOT NULL)`
+  );
+
+  await client.query(
+    `CREATE TABLE authors (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(30) NOT NULL)`
+  );
+
+  await client.query(
+    `CREATE TABLE books (id SERIAL PRIMARY KEY NOT NULL, title VARCHAR(30) NOT NULL, userid INTEGER, authorid INTEGER, categoryid INTEGER, FOREIGN KEY(userid) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY(authorid) REFERENCES authors(id) ON DELETE CASCADE, FOREIGN KEY(categoryid) REFERENCES categories(id) ON DELETE CASCADE`
+  );
+
+  await client.query(
+    `CREATE TABLE descriptions (id SERIAL PRIMARY KEY NOT NULL, description VARCHAR(10000) NOT NULL, bookid INTEGER UNIQUE, FOREIGN KEY(bookid) REFERENCES books(id) ON DELETE CASCADE)`
+  );
+
+  await client.query(
+    `CREATE TABLE reviews (id SERIAL PRIMARY KEY NOT NULL, message VARCHAR(10000) NOT NULL, userid INTEGER, bookid INTEGER, FOREIGN KEY(userid) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY(bookid) REFERENCES books(id) ON DELETE CASCADE)`
+  );
 
   client.end();
 };
@@ -33,7 +57,18 @@ export const createItems = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
+  await client.query(`INSERT INTO users (name) VALUES ('Maryna')`);
+  await client.query(`INSERT INTO categories (name) VALUES ('роман')`);
+  await client.query(`INSERT INTO authors (name) VALUES ('Виктор Пелевин')`);
+  await client.query(
+    `INSERT INTO books (title, userid, authorid, categoryid) VALUES ('Empire V', 1, 1, 1)`
+  );
+  await client.query(
+    `INSERT INTO descriptions (description, bookid) VALUES ('Миром правят вампиры. Не вечно молодые, романтичные юноши и девушки с бессонными глазами и кровавыми губами, а вполне обыкновенные мужчины и женщины со следами жизненного цинизма на лице. Им одним открыт секрет "гламура" и "дискурса". И они - настоящая мировая элита, вербующая в свои ряды прохожих одним легким укусом. Выбор может пасть на каждого...', 1)`
+  );
+  await client.query(
+    `INSERT INTO reviews (message, userid, bookid) VALUES ('Пелевин как всегда, ухватил то что витает в воздухе – и воплотил в книгу. Ай, молодец!', 1, 1)`
+  );
 
   client.end();
 };
